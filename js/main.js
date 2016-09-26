aa3b26f36fd6bde262c5f7268ab10c73c2ad04f3
Colin McKeon <14mckeon@gmail.com>
2016-09-26 16:12:13 -0400

    add zero padding and change bkgrnd color

--- js/main.js
+++ js/main.js
@@ -2,5 +2,26 @@
 
 (function(){
   "use strict";
-  
+
+
+function clock(){
+
+
+
+var hours = document.getElementById('hours');
+var minutes = document.getElementById('minutes');
+var seconds = document.getElementById('seconds');
+var date = new Date();
+
+hours.textContent = date.getHours();
+minutes.textContent = date.getMinutes();
+seconds.textContent = date.getSeconds();
+
+} /*end of clock function*/
+
+clock();
+setInterval(clock, 1000)
+
+console.log(clock);
+
 }());
